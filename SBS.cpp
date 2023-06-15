de6456bdb4df56b09f686693d070ed4b4faf838a
meghana0601 <125550286+meghana0601@users.noreply.github.com>
2023-06-15 22:53:27 +0530

    Banking.cpp

--- SBS.cpp
+++ SBS.cpp
@@ -1,244 +1,100 @@
-//Developer: Siddhartha Sarkar
-#include<iostream>
-#include<process.h>
-#include<fstream>
-#include <stdlib.h>
+#include <iostream>
+#include <vector>
+#include <string>
+
 using namespace std;
 
-class head
-{
-	char Iname[50][50];
+class Account {
+private:
+    string accountNumber;
+    string accountHolderName;
+    double balance;
 
 public:
-	int totalitems;
-	float Qty[3];
-	float price[3];
-	int vatprice[3];
-	int tprice[3];
-	void input();
-	void output();
+    Account(string number, string name, double initialBalance) {
+        accountNumber = number;
+        accountHolderName = name;
+        balance = initialBalance;
+    }
+
+    void deposit(double amount) {
+        balance += amount;
+        cout << "Deposit of $" << amount << " successful. New balance: $" << balance << endl;
+    }
+
+    void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            cout << "Withdrawal of $" << amount << " successful. New balance: $" << balance << endl;
+        } else {
+            cout << "Insufficient funds. Withdrawal failed." << endl;
+        }
+    }
+
+    void display() {
+        cout << "Account Number: " << accountNumber << endl;
+        cout << "Account Holder Name: " << accountHolderName << endl;
+        cout << "Balance: $" << balance << endl;
+    }
 };
-class vat:public head
-{
-	float vats;
+
+class Bank {
+private:
+    vector<Account> accounts;
+
 public:
-	void vatcal();
-	void outputs();
-	void sum();
+    void addAccount(Account account) {
+        accounts.push_back(account);
+        cout << "Account added successfully." << endl;
+    }
+
+    void removeAccount(string accountNumber) {
+        for (int i = 0; i < accounts.size(); i++) {
+            if (accounts[i].getAccountNumber() == accountNumber) {
+                accounts.erase(accounts.begin() + i);
+                cout << "Account removed successfully." << endl;
+                return;
+            }
+        }
+        cout << "Account not found." << endl;
+    }
+
+    Account* findAccount(string accountNumber) {
+        for (int i = 0; i < accounts.size(); i++) {
+            if (accounts[i].getAccountNumber() == accountNumber) {
+                return &accounts[i];
+            }
+        }
+        return nullptr;
+    }
 };
 
-//******************************************************************
-//	    INPUT FUNCTION
-//******************************************************************
+int main() {
+    Bank bank;
 
-void head::input()
-{
-	system("CLS");
-	cout<<"\nEnter number of items= ";
-	cin>>totalitems;
+    Account acc1("123456", "John Doe", 1000.0);
+    bank.addAccount(acc1);
 
-	for(int i=0; i<totalitems; i++)
-	{
-		cout<<"\nEnter name of item "<<i+1<<": ";
-		cin>>Iname[i];
-		cout<<"Enter quantity: ";
-		cin>>Qty[i];
-		cout<<"Enter price of item "<<i+1<<": ";
-		cin>>price[i];
-		tprice[i]=Qty[i]*price[i];
-	}
-}
+    Account acc2("789012", "Jane Smith", 500.0);
+    bank.addAccount(acc2);
 
-//******************************************************************
-//	   OUTPUT FUNCTION
-//******************************************************************
+    Account* acc3 = bank.findAccount("123456");
+    if (acc3 != nullptr) {
+        acc3->deposit(200.0);
+        acc3->withdraw(300.0);
+        acc3->display();
+    } else {
+        cout << "Account not found." << endl;
+    }
 
+    Account* acc4 = bank.findAccount("999999");
+    if (acc4 != nullptr) {
+        acc4->display();
+    } else {
+        cout << "Account not found." << endl;
+    }
 
+    bank.removeAccount("789012");
 
-void head::output()
-{
-	int a;
-
-	ifstream infile("COUNT.TXT");
-	infile>>a;
-
-	ofstream outfile("COUNT.TXT");
-	a+=1;
-	outfile<<a;
-	outfile.close();
-
-	{ofstream outfile("HIS.TXT", ios::app);
-	outfile<<endl<<"Bill No.: "<<a<<endl;
-	outfile<<"------------------------------------------------------------------------"<<endl;
-cout<<"\n";
-	cout<<"Name of Item\tQuantity   Price  Total Price\n";
-	for(int i=0;i<totalitems;i++)
-	{
-		outfile<<"Name: "<<Iname[i]<<" Qty: "<<Qty[i]<<" Price: "<<tprice[i]<<endl;
-		cout<<Iname[i]<<"\t\t"<<Qty[i]<<"\t   "<<price[i]<<"\t   "<<tprice[i]<<'\n';
-	}
-
-	outfile<<"------------------------------------------------------------------------"<<endl;
-	outfile.close();
-	}
-}
-
-
-//******************************************************************
-//	   VAT CALCULATION
-//******************************************************************
-
-void vat::vatcal()
-{
-	input();
-	for(int i=0;i<totalitems;i++)
-	{
-		if(price[i]<=100.00)
-		{
-			vatprice[i]=tprice[i]+(0.03*tprice[i]);
-		}
-		else
-		{
-			vatprice[i]=tprice[i]+(0.1*tprice[i]);
-		}
-	}
-}
-//******************************************************************
-//	    VAT OUTPUTS
-//******************************************************************
-
-void vat::outputs()
-{
-	output();
-
-	float cash=0,sum=0,qty=0,sumt=0;
-
-	for(int i=0;i<totalitems;i++)
-	{
-	       sumt+=tprice[i];
-		   sum+=vatprice[i];
-		   qty+=Qty[i];
-	}
-	cout<<"\nTotal:";
-	cout<<"\n------------------------------------------------------------------------------";
-	cout<<"\n\tQuantity= "<<qty<<"\t\t Sum= "<<sumt<<"\tWith Vat:"<<sum;
-	cout<<"\n------------------------------------------------------------------------------";
-
-pay:
-
-	cout<<"\n\n\t\t\t****PAYMENT SUMMARY****\n";
-	cout<<"\n\t\t\tTotal cash given: ";
-	cin>>cash;
-
-	if(cash>=sum)
-		cout<<"\n\t\t\tTotal cash repaid: "<<cash-sum<<'\n';
-		
-	else
-	{	cout<<"\n\t\t\tCash given is less than total amount!!!";
-
-	goto pay;
-	}
-}
-
-
-//******************************************************************
-//	    PROTECTION PASSWORD
-//******************************************************************
-
-int passwords()
-{
-
-	char p1,p2,p3;
-
-	cout<<"\n\n\n\n\n\n\t\t\tENTER THE PASSWORD: ";
-
-	cin>>p1;
-	cout<<"*";
-	cin>>p2;
-	cout<<"*";
-	cin>>p3;
-	cout<<"*";
-
-	if ((p1=='s'||p1=='S')&&(p2=='i'||p2=='I')&&(p3=='d'||p3=='D'))
-
-		return 1;
-
-	else
-		return 0;
-}
-// END of Password.
-
-//****************************************************************
-//    	THE MAIN FUNCTION OF PROGRAM
-//****************************************************************
-
-
-int main()
-{
-	vat obj;
-	char opt, ch;
-	int a=1;
-	ifstream fin;
-
-	a==passwords();
-	if(!a)
-	{
-		for(int i=0;i<2;i++)
-		{
-			cout<<"\nWrong password try once more\n";
-			if(passwords())
-			{
-				goto last;
-			}
-			else
-			{
-				cout<<"\n\n\t\t\t all attempts failed.....";
-				cout<<"\n\n\n\t\t\t see you.................. ";
-				exit(0);
-			}
-
-		}
-		cout<<"\t\t\t sorry all attempts failed............. \n \t\t\tinactive";
-			 }
-	else{
-last:;
-
-
-	 do{
-start:
-	system("PAUSE");
-	system("CLS");
-	cout<<"\n\n\t\t\t------------------------------";
-	cout<<"\n\t\t\tShop Billing Management System";
-	cout<<"\n\t\t\t------------------------------";
-	 cout<<"\n\n\t\t\tWhat you want to do?";
-	 cout<<"\n\t\t\t1.\tTo enter new entry\n\t\t\t2.\tTo view previous entries\n\t\t\t3.\tExit\n";
-	 cout<<"\n\nEnter your option: ";
-	 cin>>opt;
-	 switch(opt)
-	 {
-	 case'1':
-		 obj.vatcal();
-
-		 obj.outputs();
-		 goto start;
-	 case'2':
-
-		 fin.open("HIS.TXT", ios::in);
-		 while(fin.get(ch))
-		 {
-			 cout<<ch;
-		 }
-		 fin.close();
-
-		 goto start;
-	 case'3':
-		 exit(0);
-	 default:
-		 cout<<"\a";
-	 }
-
-	 }while(opt!=3);
-	}
-	return 0;
+    return 0;
 }
